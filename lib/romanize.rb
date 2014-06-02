fa520b2eae1161586a784fb376e87cc2528e2ad1
itgsod-pontus-egebergjacob <pontus.egebergjacobson@elev.it-gymnasiet.se>
2014-06-02 13:29:23 +0200

    added the list and the loop to make the test show green

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -2,6 +2,19 @@ def romanize(input)
   raise ArgumentError, 'can not encode negative number' if input < 0
   raise ArgumentError, 'can not encode zero' if input == 0
 
-  return "hej"
+  romankeys = {1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV' ,1 => 'I'}
+  currentValue = input
+  output = ''
 
+  until currentValue == 0
+    romankeys.each do |key,value|
+      if currentValue >= key
+        output += value
+        currentValue -= key
+        break
+      end
+    end
+  end
+
+  return output
 end
